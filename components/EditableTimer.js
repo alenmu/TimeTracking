883b7065a79219f7a4ca635d3b2230a0c996ba51
alenmu <amujkic1990@hotmail.com>
2020-12-19 19:46:09 +0100

    added update EditableTimer

--- components/EditableTimer.js
+++ components/EditableTimer.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import TimerForm from "./TimerForm";
 import Timer from "./Timer";
 
@@ -6,11 +7,44 @@ export default class EditableTimer extends React.Component {
   state = {
     editFormOpen: false,
   };
+
+  handleEditPress = () => {
+    this.openForm();
+  };
+
+  handleFormClose = () => {
+    this.closeForm();
+  };
+
+  handleSubmit = (timer) => {
+    const { onFormSubmit } = this.props;
+
+    onFormSubmit(timer);
+    this.closeForm();
+  };
+
+  closeForm = () => {
+    this.setState({ editFormOpen: false });
+  };
+
+  openForm = () => {
+    this.setState({ editFormOpen: true });
+  };
+
   render() {
     const { id, title, project, elapsed, isRunning } = this.props;
     const { editFormOpen } = this.state;
+
     if (editFormOpen) {
-      return <TimerForm id={id} title={title} project={project} />;
+      return (
+        <TimerForm
+          id={id}
+          title={title}
+          project={project}
+          onFormSubmit={this.handleSubmit}
+          onFormClose={this.handleFormClose}
+        />
+      );
     }
     return (
       <Timer
@@ -19,6 +53,7 @@ export default class EditableTimer extends React.Component {
         project={project}
         elapsed={elapsed}
         isRunning={isRunning}
+        onEditPress={this.handleEditPress}
       />
     );
   }
